90db4b7c53970ea91cc33e116d84debd1d7f168c
Rithika Reddy Beravelly <161623968+RithikaReddyB@users.noreply.github.com>
2025-09-01 01:48:36 +0530

    Add files via upload

---
+++ doencfileserver.c
@@ -0,0 +1,109 @@
+// ===============================================================================================
+// Assignment 3 submission
+// Name: Darapu Adhithya Shiva Kumar Reddy
+// Roll No:22CS30019
+// Drive Link For Pcap File: https://drive.google.com/drive/folders/1W7BKbTKZimXsz07YDmXKnCOPxh8IJBhn?usp=sharing
+// ===============================================================================================
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+
+#define PORT 20000
+#define BUF_SIZE 100
+
+void encrypt_text(char *buffer, char *key) {
+    for (int i = 0; buffer[i] != '\0'; i++) {
+        if (isalpha(buffer[i])) {
+            if (isupper(buffer[i])) {
+                buffer[i] = key[buffer[i] - 'A'];
+            } else {
+                buffer[i] = tolower(key[buffer[i] - 'a']);
+            }
+        }
+    }
+}
+
+int main() {
+    int sockfd, newsockfd, clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    char buffer[BUF_SIZE];
+    char key[27];
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Cannot create socket");
+        exit(1);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.127.127.1");
+    serv_addr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Bind failed");
+        exit(1);
+    }
+
+    listen(sockfd, 5);
+    printf("Server listening on port %d...\n", PORT);
+
+    while (1) {
+        clilen = sizeof(cli_addr);
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        if (fork() == 0) { // Child process
+            close(sockfd);
+            char client_filename[50];
+            snprintf(client_filename, 50, "%s.%d.txt", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+
+            recv(newsockfd, key, 27, 0);
+
+            FILE *temp = fopen(client_filename, "w");
+            if (!temp) {
+                perror("File creation failed");
+                close(newsockfd);
+                exit(1);
+            }
+
+            while (1) {
+                int bytes_received = recv(newsockfd, buffer, BUF_SIZE - 1, 0);
+                if (bytes_received <= 0) break;
+                buffer[bytes_received] = '\0'; // Null-terminate the buffer
+                fputs(buffer, temp);
+            }
+            fclose(temp);
+
+            FILE *input = fopen(client_filename, "r");
+            char enc_filename[60];
+            snprintf(enc_filename, 60, "%s.enc", client_filename);
+            FILE *output = fopen(enc_filename, "w");
+
+            while (fgets(buffer, BUF_SIZE, input)) {
+                encrypt_text(buffer, key);
+                fputs(buffer, output);
+            }
+            fclose(input);
+            fclose(output);
+
+            FILE *enc_file = fopen(enc_filename, "r");
+            while (fgets(buffer, BUF_SIZE, enc_file)) {
+                send(newsockfd, buffer, strlen(buffer), 0);
+            }
+            fclose(enc_file);
+
+            close(newsockfd);
+            exit(0);
+        }
+        close(newsockfd);
+    }
+    return 0;
+}
---
+++ retrieveencfileclient.c
@@ -0,0 +1,97 @@
+// ===============================================================================================
+// Assignment 3 submission
+// Name: Darapu Adhithya Shiva Kumar Reddy
+// Roll No:22CS30019
+// Drive Link For Pcap File: https://drive.google.com/drive/folders/1W7BKbTKZimXsz07YDmXKnCOPxh8IJBhn?usp=sharing
+// ===============================================================================================
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define PORT 20000
+#define BUF_SIZE 100
+
+int main() {
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    char buffer[BUF_SIZE], key[27], filename[100], enc_filename[100];
+    FILE *file;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Cannot create socket");
+        exit(1);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    inet_aton("127.127.127.1", &serv_addr.sin_addr);
+    serv_addr.sin_port = htons(PORT);
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Cannot connect to server");
+        exit(1);
+    }
+
+    while (1) {
+        printf("Enter filename: ");
+        scanf("%s", filename);
+
+        file = fopen(filename, "r");
+        while (!file) {
+            printf("NOTFOUND %s\nEnter a valid filename: ", filename);
+            scanf("%s", filename);
+            file = fopen(filename, "r");
+        }
+
+        printf("Enter encryption key (26 characters): ");
+        scanf("%s", key);
+        while (strlen(key) < 26) {
+            printf("Invalid key. Enter again: ");
+            scanf("%s", key);
+        }
+
+        send(sockfd, key, 27, 0);
+
+        while (fgets(buffer, BUF_SIZE, file)) {
+            send(sockfd, buffer, strlen(buffer), 0);
+        }
+        fclose(file);
+        shutdown(sockfd, SHUT_WR);
+
+        snprintf(enc_filename, sizeof(enc_filename), "%s.enc", filename);
+
+        FILE *enc_file = fopen(enc_filename, "w");
+
+        while (1) {
+            int bytes_received = recv(sockfd, buffer, BUF_SIZE - 1, 0);
+            if (bytes_received <= 0) break;
+            buffer[bytes_received] = '\0'; // Null-terminate the buffer
+            fputs(buffer, enc_file);
+        }
+        fclose(enc_file);
+
+        printf("File encrypted: %s -> %s\n", filename, enc_filename);
+
+        printf("Do you want to encrypt another file? (yes/no): ");
+        scanf("%s", buffer);
+        if (strcmp(buffer, "no") == 0) {
+            break;
+        }
+        
+        // Reconnect for the next file
+        close(sockfd);
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            perror("Cannot create socket");
+            exit(1);
+        }
+        if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+            perror("Cannot connect to server");
+            exit(1);
+        }
+    }
+    close(sockfd);
+    return 0;
+}
